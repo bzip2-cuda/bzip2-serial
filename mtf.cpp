f27631b23a288b88be09607202eac957cae6214d
anushree <anushree@anushree-laptop.(none)>
2010-12-14 15:45:33 +0530

    Timing added in mtf serial

--- mtf.cpp
+++ mtf.cpp
@@ -10,28 +10,37 @@ int main()
 {
 	string list, word;
 	int counter, index;
+	time_t begin, end;
+	cout <<"Enter word to perform mtf with ";
 	cin >> word;
-	for (counter = 0; counter != 256; counter++)
+	int k = 0;
+	begin = time(NULL);
+	for (; k < 1000; k++)
 	{
-		string list1;
-		list1 = char(counter);
-		list = list + list1;
-	}
-	//Comparing character with the string
-	for (counter = 0; counter < word.length(); counter++)
-	{
-		for (index = 0; ; index++)                 //scan the whole array, no need of end condition
+		for (counter = 0; counter != 256; counter++)
 		{
-			if (word[counter] == list[index]) //till we found it
-				break;    //then, break the for
+			string list1;
+			list1 = char(counter);
+			list = list + list1;
 		}
-		//Putting found byte in front
-		for ( ; index != 0; index--)      //scan from the position of the byte to 0
-		{	
-			list[index] = list[index-1];  //move all of them to the right
+		//Comparing character with the string
+		for (counter = 0; counter < word.length(); counter++)
+		{
+			for (index = 0; ; index++)                 //scan the whole array, no need of end condition
+			{
+				if (word[counter] == list[index]) //till we found it
+					break;    //then, break the for
+			}
+			//Putting found byte in front
+			for ( ; index != 0; index--)      //scan from the position of the byte to 0
+			{	
+				list[index] = list[index-1];  //move all of them to the right
+			}
+			list[0] = word[counter];                      //update the order 0
 		}
-		list[0] = word[counter];                      //update the order 0
+		for (counter = 0; counter <= word.length() && k == 0; counter++)
+			cout << counter << "\t" << list[counter] << endl;
 	}
-	for (counter = 0; counter <= word.length(); counter++)
-		cout << counter << "\t" << list[counter] << endl;
+	end = time(NULL);
+	cout <<"Time taken = " <<difftime(end, begin) << endl;
 }
