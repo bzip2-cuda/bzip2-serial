4a36a42c535756e0819f26abcc50c8f7a532a585
Anirudh Ramachandran <anirudhr1989@gmail.com>
2011-03-24 09:52:21 +0530

    Update || Adding file-handler src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <time.h>
+#include <fstream.h>
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) { help(argv[0]); return 1; }
+  std::vector<std::string> file;
+  std::string line;
+  file.clear();
+  std::ifstream infile (argv[1], std::ios_base::in);
+  while (getline(infile, line, '\n'))
+  {
+    file.push_back (line);
+  }
+
+  std::cout << "Read " << file.size() << " lines.\n";
+
+  return 0;
+}
+
